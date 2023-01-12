e87018ee23f03200f66af2332b80e0b5dee2abbf
hasli <20180010@students.htl-perg.ac.at>
2023-01-12 08:44:31 +0100

    close #6

---
+++ succerWM/src/components/team-component.ts
@@ -0,0 +1,77 @@
+import { group } from "console"
+import { html, render } from "lit-html"
+import { Module } from "module"
+import { Group, Player } from "../model/model"
+import { Team } from "../model/model"
+import store from "../model/store"
+import { mergeMap, filter} from "rxjs"
+
+
+const tableTemplate = html`
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <table class="w3-table w3-striped w3-bordered">
+        <thead>
+            <tr>
+            <th>Playername</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+        <button id="backbutton" >Back</button>
+    </table>
+`
+const rowTemplate = (player: Player) => html`
+    <td>${player.playerName}</td>
+`
+
+
+class TeamComponent extends HTMLElement {
+    static get observedAttributes() {
+        return ["team"]
+    }
+
+    private root: ShadowRoot
+    constructor() {
+        super()
+        this.root = this.attachShadow({ mode: "closed" })
+    }
+
+    private team: string
+    attributeChangedCallback(name: string, oldValue: string, value: string) {
+        console.log("TODO: display team", name)
+
+    
+        if (name === "team"){
+            this.team = value
+        }
+        
+        this.render()
+    }
+
+    
+
+    connectedCallback() {
+        this.render()
+    }
+
+    private render() {
+        store.pipe(mergeMap(group => group.groups), mergeMap(group => group.teams), filter(team => team.countryName === this.team))
+        .subscribe(async t => {
+            render(tableTemplate, this.root)
+            this.root.getElementById("backbutton").onclick = () => {
+                    const event = new CustomEvent("back")
+                    this.dispatchEvent(event)
+                }
+            const body = this.root.querySelector("tbody")
+            body.innerHTML = ""
+            t.player.forEach(p => {
+                console.log(p)
+                const row = body.insertRow()
+                render(rowTemplate(p), row)
+            })
+        })
+        
+
+    }
+}
+
+customElements.define("team-component", TeamComponent)
