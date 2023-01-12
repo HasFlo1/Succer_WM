df6bfb83a7ba85f12b550fbce33b3828eefcae1b
20180098 <20180098@students.htl-perg.ac.at>
2023-01-12 09:29:42 +0100

    updated json

--- succerWM/src/components/team-component.ts
+++ succerWM/src/components/team-component.ts
@@ -1,81 +1,3 @@
-<<<<<<< HEAD
-import { group } from "console"
-import { html, render } from "lit-html"
-import { Module } from "module"
-import { Group, Player } from "../model/model"
-import { Team } from "../model/model"
-import store from "../model/store"
-import { mergeMap, filter} from "rxjs"
-
-
-const tableTemplate = html`
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <table class="w3-table w3-striped w3-bordered">
-        <thead>
-            <tr>
-            <th>Playername</th>
-            </tr>
-        </thead>
-        <tbody></tbody>
-        <button id="backbutton">Back</button>
-    </table>
-`
-const rowTemplate = (player: Player) => html`
-    <td>${player.playerName}</td>
-`
-
-
-class TeamComponent extends HTMLElement {
-    static get observedAttributes() {
-        return ["team"]
-    }
-
-    private root: ShadowRoot
-    constructor() {
-        super()
-        this.root = this.attachShadow({ mode: "closed" })
-    }
-
-    private team: string
-    attributeChangedCallback(name: string, oldValue: string, value: string) {
-        console.log("TODO: display team", name)
-
-    
-        if (name === "team"){
-            this.team = value
-        }
-        
-        this.render()
-    }
-
-    
-
-    connectedCallback() {
-        this.render()
-    }
-
-    private render() {
-        store.pipe(mergeMap(group => group.groups), mergeMap(group => group.teams), filter(team => team.countryName === this.team))
-        .subscribe(async t => {
-            render(tableTemplate, this.root)
-            this.root.getElementById("backbutton").onclick = () => {
-                    const event = new CustomEvent("back")
-                    this.dispatchEvent(event)
-                }
-            const body = this.root.querySelector("tbody")
-            body.innerHTML = ""
-            t.player.forEach(p => {
-                console.log(p)
-                const row = body.insertRow()
-                render(rowTemplate(p), row)
-            })
-        })
-        
-
-    }
-}
-
-=======
 import { group } from "console"
 import { html, render } from "lit-html"
 import { Module } from "module"
@@ -152,5 +74,4 @@ class TeamComponent extends HTMLElement {
     }
 }
 
->>>>>>> e87018ee23f03200f66af2332b80e0b5dee2abbf
 customElements.define("team-component", TeamComponent)
